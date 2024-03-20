055e514a2901875a4bb8b4cf9635caed97765e55
Wannaaaaaaa <113595533+Wannaaaaaaa@users.noreply.github.com>
2024-03-20 17:39:44 +0100

    Create Esercizio1.java

---
+++ Esercizio1.java
@@ -0,0 +1,8 @@
+public class Esercizio1 {
+	public static void main(String[] args) {
+		int base = 4;
+		int altezza = 5;
+		double area = base * altezza;
+		System.out.println("L'area del rettangolo di base 4 e altezza 5 corrisponde a: " + area);
+	}
+}
