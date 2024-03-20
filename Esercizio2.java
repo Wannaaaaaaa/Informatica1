22a22bfe2d78e30f71574612212f37b0698c334d
Wannaaaaaaa <113595533+Wannaaaaaaa@users.noreply.github.com>
2024-03-20 17:40:30 +0100

    Create Esercizio2.java

---
+++ Esercizio2.java
@@ -0,0 +1,8 @@
+public class Esercizio2 {
+	public static void main(String[] args) {
+		char car = 'a';
+		int incremento = 1;
+		car+=incremento;
+		System.out.println("Valore finale di car: " + car);
+	}
+}
